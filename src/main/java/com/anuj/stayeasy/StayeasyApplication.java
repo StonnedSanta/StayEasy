8b99b2ae94ec56e981606f3b2097effe1d12148a
Anuj Srivastava <anujshrivastav128@gmail.com>
2026-07-26 23:53:15 +0530

    feat: implement CommandLineRunner to test UserService class features

--- src/main/java/com/anuj/stayeasy/StayeasyApplication.java
+++ src/main/java/com/anuj/stayeasy/StayeasyApplication.java
@@ -1,13 +1,35 @@
 package com.anuj.stayeasy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.anuj.stayeasy.entity.User;
+import com.anuj.stayeasy.service.UserService;
 
 @SpringBootApplication
 public class StayeasyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StayeasyApplication.class, args);
+
+	}
+
+	@Bean
+	CommandLineRunner test(UserService userService) {
+
+		return args -> {
+			User user = new User();
+
+			user.setFullName("Anish S");
+			user.setEmail("anish21@gmail.com");
+			user.setPassword("123654");
+
+			User savedUser = userService.register(user);
+
+			System.out.println(savedUser);
+		};
 	}
 
 }
